7d01869feee6614a66921cce4f38eadc566ab759
wisedev73 <48600478+BrabusS73@users.noreply.github.com>
2023-04-06 10:38:14 -0700

    Update tenant.py

--- tenant.py
+++ tenant.py
@@ -1,9 +1,14 @@
+ten = {}
 class Tenant:
-    def display_tenants(self):
-        pass
+    def display_tenants():
+        if (len(ten) == 0):
+            print("The list is empty.")
+        else:
+            for i in ten:
+                print("Name:", i, ", Room number:", ten[i])
 
-    def add_tenant(self):
-        pass
+    def add_tenant(tname, troomnum):
+        ten.update({tname: troomnum})
 
-    def remove_tenant(self):
-        pass
+    def remove_tenant(remname):
+        ten.pop(remname)
