c7e6e510e4395bcbf462b16ae6e6016a1f7d94b0
wisedev73 <48600478+BrabusS73@users.noreply.github.com>
2023-04-10 12:12:13 -0700

    Update tenant.py

--- tenant.py
+++ tenant.py
@@ -1,21 +1,21 @@
-tenant = {}
+tenant_list = {}
 class Tenant:
     def display_tenants():
     #Displays the dictionary that contains tenants' information
-        if (len(tenant) == 0):
+        if (len(tenant_list) == 0):
             print("The list is empty.")
         else:
-            for i in tenant:
-                print("Name:", i, ", Room number:", tenant[i])
+            for i in tenant_list:
+                print("Name:", i, ", Room number:", tenant_list[i])
 
     def add_tenant(tname, troomnum):
     #Adds tenants' information to the tenant dictionary
-        tenant.update({tname: troomnum})
+        tenant_list.update({tname: troomnum})
 
     def remove_tenant(remname):
     #Removes tenants' information from the tenant dictionary
-        if (remname in tenant):
-            tenant.pop(remname)
+        if (remname in tenant_list):
+            tenant_list.pop(remname)
         else:
             print("Error: The tenant name you are looking for is not in the list.")
         
