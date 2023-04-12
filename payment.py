6264ac54c60aad141b7fd66277864abd957bf5ba
wisedev73 <48600478+BrabusS73@users.noreply.github.com>
2023-04-12 12:26:13 -0700

    Update payment.py

--- payment.py
+++ payment.py
@@ -1,5 +1,12 @@
+from tabulate import tabulate
 class Payment:
-    payment = 0
-
-    def add_payment(self):
-        pass
+    def __init__(self, name):
+        self.name = name
+        self.payment_list = []
+        self.months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    def add_payment(self, pay_amount):
+        self.payment_list.append(pay_amount)
+    def print_paylist(self):
+        print(self.name, "'s payment list:")
+        table = [["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"], self.payment_list]
+        print(tabulate(table))
