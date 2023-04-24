cee1b651ce3fbb3eb399a1a27087bfe1c9f221e7
ShivManek <126350807+ShivManek@users.noreply.github.com>
2023-04-23 22:14:56 -0700

    Update tenant.py

--- tenant.py
+++ tenant.py
@@ -8,14 +8,25 @@ class Tenant:
             for i in tenant_list:
                 print("Name:", i, ", Room number:", tenant_list[i])
 
-    def add_tenant(tname, troomnum):
-    #Adds tenants' information to the tenant dictionary
-        tenant_list.update({tname: troomnum})
+    def add_tenant(self,name, roomnum):
+        tenant_file = open(self.tenant_list_file, 'a')
+        tenant_file.write(name + "\n")
+        tenant_file.close()
 
-    def remove_tenant(remname):
-    #Removes tenants' information from the tenant dictionary
-        if (remname in tenant_list):
-            tenant_list.pop(remname)
-        else:
-            print("Error: The tenant name you are looking for is not in the list.")
-        
+        room_file = open(self.room_list_file, 'a')
+        room_file.write(str(roomnum)+ "\n")
+        room_file.close()
+
+    def remove_tenant(self,name,room):
+        with open("Tenantlist.txt", "r") as f:
+            lines = f.readlines()
+        with open("Tenantlist.txt", "w") as f:
+            for line in lines:
+                if line.strip() != name:
+                    f.write(line)
+        with open("Roomlist.txt", "r") as f:
+            lines = f.readlines()
+        with open("Roomlist.txt", "w") as f:
+            for line in lines:
+                if line.strip() != room:
+                    f.write(line)
