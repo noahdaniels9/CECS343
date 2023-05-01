67ee028c5f2171a2be06b344ff4496d973de1a3e
shivkumar M <shivkumarmanek@gmail.com>
2023-05-01 11:59:00 -0700

    add room and remove room method done, updated Rooms.txt

--- room.py
+++ room.py
@@ -1,4 +1,8 @@
-room_Dict = {}
+import csv
+from tabulate import tabulate
+from helper import print_color, validate_input
+
+room_list = []
 class Room:
     number: int = 0
     rent: float = 0
@@ -14,21 +18,43 @@ class Room:
 
     def adjust_rent(self):
         try:
-            for i in room_Dict:
-                print(i, room_Dict[i])
+            for i in room_list:
+                print(i, room_list[i])
             roomNum = input("For what room do you want to change the rent amount?:")
             newRent = input("Enter the rent amount that you want to adjust to: $")
-            room_Dict[roomNum] = newRent
+            room_list[roomNum] = newRent
         except:
             print("An unexpected error has occured. Try again.")
 
     @staticmethod
     def add_room():
-        pass
+        """Add a new room dictionary in memory"""
+        print_color("ADDING A NEW ROOM", "third")
+        print_color("Enter 0 to quit", "info")
+
+        room_num = input("Name: ")
+        if room_num == "0":
+            return
+
+        room_list.append(Room(room_num))
 
     @staticmethod
     def remove_room():
-        pass
+        """Remove a room from the tenant dictionary in memory."""
+        print_color("REMOVING A ROOM", "third")
+        print_color("Enter 0 to quit", "info")
+
+        name_to_delete = input("Room Number: ")
+        if name_to_delete == "0":
+            return
+
+        for i in room_list:
+            if i.name == name_to_delete:
+                room_list.remove(i)
+                print_color("Room removed\n", "success")
+                return
+
+        print_color("Room Number not found\n", "error")
 
     @staticmethod
     def read_from_database(room_filename):
