776fb83207d0bd1db3a6b801734a45d74545e8f2
Vinh Huynh <35861939+vinhq-huynh@users.noreply.github.com>
2023-05-02 09:29:16 -0700

    Added categories

---
+++ category.py
@@ -0,0 +1,19 @@
+import csv
+
+category_list = []
+
+
+def read_from_database(filename):
+    """Read all category information from the database into memory"""
+    with open(filename, 'r') as fp:
+        for line in fp:
+            category_list.append(line.strip())
+
+
+def write_to_database(filename):
+    """Write all category information from memory into the database"""
+    with open(filename, 'w') as fp:
+        for category in category_list:
+            fp.write(category)
+
+
